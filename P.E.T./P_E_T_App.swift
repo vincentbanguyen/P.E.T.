ab4ae894d6b7ba52d730092f6f2db7a94d37f48c
Vincent Nguyen <vincentbanguyen@gmail.com>
2022-04-30 10:22:26 -0500

    Initial Commit

---
+++ P.E.T./P_E_T_App.swift
@@ -0,0 +1,17 @@
+//
+//  P_E_T_App.swift
+//  P.E.T.
+//
+//  Created by Vincent Nguyen on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct P_E_T_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
